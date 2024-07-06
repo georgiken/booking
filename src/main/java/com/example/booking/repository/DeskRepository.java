88950ea42a047fdf7c77590bf9f4fde1b2276351
georgiken <georgiy.voronin.05@mail.ru>
2024-07-06 17:18:20 +0300

    desk repository init

---
+++ src/main/java/com/example/booking/repository/DeskRepository.java
@@ -0,0 +1,8 @@
+package com.example.booking.repository;
+
+import com.example.booking.entity.Desk;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeskRepository extends JpaRepository<Desk, Long> {
+
+}
